e71b1cde91d1400146e4a931b2764d4b9b4b8fa9
Mario Jimenez <mariojimenez1077@gmail.com>
2016-10-19 17:24:19 -0500

    Server de texto con nodemon

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 // Cargando el modulo http
 var http = require('http');
+//cargando la libreria de colores
+var colors = require("colors");
+//generando tema
+colors.setTheme({
+silly: 'rainbow',
+input: 'grey',
+verbose: 'cyan',
+prompt: 'grey',
+info: 'green',
+data: 'grey',
+help: 'cyan',
+warn: ['yellow', 'bgwhite'],
+debug: 'blue',
+error: 'red',
+archivement: 'rainbow'
+})
+//obteniendo configuraciones
+var IP = '127.0.0.1';
+var PORT = 3000;
 // creando el server
 var server = http.createServer(function(req,res){
     res.writeHead(
@@ -13,6 +32,6 @@ var server = http.createServer(function(req,res){
     res.end();
 });
 // Poniendo a escuchar al server
-server.listen(3000,'127.0.0.1',function(){
-    console.log("Online" + "http://127.0.0.1:3000 ...");
+server.listen(PORT,IP,function(){
+    console.log("Online ".info + `http://${IP}:${PORT}/ ...`.info);
 });
