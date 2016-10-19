9deace04f1f7b45736c18c4c45ec1b96daf6a376
Mario Jimenez <mariojimenez1077@gmail.com>
2016-10-18 19:57:03 -0500

    Se crea servidor de texto plano monilitico

---
+++ server.js
@@ -0,0 +1,18 @@
+// Cargando el modulo http
+var http = require('http');
+// creando el server
+var server = http.createServer(function(req,res){
+    res.writeHead(
+    200,
+    {
+        'Content-Type': 'text/plain',
+        'Server' : 'Buho@0.0.0'
+    }
+    );
+    res.write('hola desde el server');
+    res.end();
+});
+// Poniendo a escuchar al server
+server.listen(3000,'127.0.0.1',function(){
+    console.log("Online" + "http://127.0.0.1:3000 ...");
+});
