7da9b9bbb6eb89907b1d0ee95b2b232777ea0e71
Mario Jimenez <mariojimenez1077@gmail.com>
2016-10-20 18:09:25 -0500

    Server statico completo

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
-    'IP' : process.env.IP || '127.0.0.1', 'PORT': process.env.PORT || 3000
+    'IP' : process.env.IP || '127.0.0.1', 'PORT': process.env.PORT || 3000, 'STATIC_PATH': './static'
 };
--- server.js
+++ server.js
@@ -1,38 +1,89 @@
 // Cargando el modulo http
 var http = require('http');
-//cargando la libreria de colores
-var colors = require("colors");
-//generando tema
+// Cargando libreri path
+var path = require('path');
+// Cargando la libreria colors
+var colors = require('colors');
+var fs = require('fs');
+// Generando un tema
 colors.setTheme({
-silly: 'rainbow',
-input: 'grey',
-verbose: 'cyan',
-prompt: 'grey',
-info: 'green',
-data: 'grey',
-help: 'cyan',
-warn: ['yellow', 'bgwhite'],
-debug: 'blue',
-error: 'red',
-archivement: 'rainbow'
-})
-//obteniendo configuraciones
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: ['yellow', 'bgWhite'],
+  debug: 'blue',
+  error: 'red',
+  achivement: 'rainbow'
+});
+// Obteniendo configruaciones
 var config = require('./config/config');
 var IP = config.IP;
 var PORT = config.PORT;
-// creando el server
+
+var counter = 0;
+// Creando el server
 var server = http.createServer(function(req,res){
-    res.writeHead(
-    200,
-    {
-        'Content-Type': 'text/plain',
-        'Server' : 'Buho@0.0.0'
+    var urlPath = req.url;
+    console.log(`> URL solicitada: ${urlPath}`.silly);
+    if(urlPath == '/'){
+        // Genera una ruta hacia el index.html
+        urlPath = path.resolve('./static/index.html');
+    }else{
+        // Genera una ruta dentro de static
+        urlPath = 
+        path.resolve(config.STATIC_PATH + urlPath);        
     }
-    );
-    res.write('hola desde el server');
-    res.end();
+    // Exratyendo la extencion de lo que
+    // vamos a servir
+    var extname = path.extname(urlPath);
+    // Seleccionar el contet-type con base
+    // en el extname
+    var contentType = 'text/plain';
+    switch(extname){
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css'
+            break;
+        case '.html':
+            contentType = 'text/html';
+            break;
+    };
+    fs.exists(urlPath,function(exists){
+        if(!exists){
+            // No  existe
+            res.writeHead(404,{
+                'Content-Type':'text/html'
+            });
+            res.end('<h1>404 NOT FOUND... :(</h1>');
+        } else{
+            // Si existe
+            // Leemos el archivo y lo servimos
+            fs.readFile(urlPath,function(err, content){
+                if(err){
+                    res.writeHead(500,{
+                        'Content-Type':'text/html'
+                    });
+                    res.end('<h1 style="color: red">500 ERROR</h1>');
+                }else{
+                    // Si pudo leer el archivo
+                    res.writeHead(200,{
+                        'Content-Type':contentType
+                    });
+                    res.end(content);
+                }
+            });
+        }
+    });
 });
-// Poniendo a escuchar al server
+// Poniendo a escuchar
+// al server
 server.listen(PORT,IP,function(){
-    console.log("Online ".info + `http://${IP}:${PORT}/ ...`.info);
+    console.log("> Server escuchando en ".info +
+    `http://${IP}:${PORT}/ ...`.info);
 });
